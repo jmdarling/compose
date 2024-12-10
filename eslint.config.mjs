36b7e808eaf8996b35c05275a9ba7a23fbe1885c
Jonathan Darling <hello@jmdarling.com>
2024-12-10 14:44:13 -0600

    chore(*): standardize pull_policy

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,6 +19,7 @@ export default [
             "container_name",
             "image",
             "build",
+            "pull_policy",
             "deploy",
             "restart",
             "command",
