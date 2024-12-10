2fe4bb587c1aac4b51646e395b5980ec61de3d73
Jonathan Darling <hello@jmdarling.com>
2024-12-10 14:38:10 -0600

    feat(*): enable linting to force consistency of structure

---
+++ eslint.config.mjs
@@ -0,0 +1,34 @@
+import eslintPluginYml from "eslint-plugin-yml";
+export default [
+  // add more generic rule sets here, such as:
+  // js.configs.recommended,
+  ...eslintPluginYml.configs["flat/standard"],
+  {
+    rules: {
+      // Empty mapping values are needed in Docker Compose files.
+      "yml/no-empty-mapping-value": "off",
+      "yml/sort-keys": [
+        "error",
+        {
+          pathPattern: "^$",
+          order: ["services", "networks", "volumes"],
+        },
+        {
+          pathPattern: "^services.*$",
+          order: [
+            "container_name",
+            "image",
+            "build",
+            "deploy",
+            "restart",
+            "command",
+            "environment",
+            "networks",
+            "ports",
+            "volumes",
+          ],
+        },
+      ],
+    },
+  },
+];
